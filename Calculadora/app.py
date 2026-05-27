6973d4c0a0284f12be8f40871db91e15cf0292a9
Caio Nunes Pereira <caionupp@gmail.com>
2026-05-27 08:06:22 -0300

    Add files via upload

--- Calculadora/app.py
+++ Calculadora/app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from calculadora import calcular
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('calculadora.html', etapas = '', resultados = '')
 
-@app.route('/calcular', methods=['POST']) #aqui o /calcular não endereça uma nova página
-def calcular_route():
-    return calcular()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return calcular()
+    return render_template('calculadora.html', etapas = '', resultados = '')
 
 if __name__ == "__main__":
     app.run(debug=True)
